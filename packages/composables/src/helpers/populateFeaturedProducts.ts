f91022f7fd29f83ca66aa71191c9e1a95c9f906b
Sam <sam@binshops.com>
2021-08-24 06:17:05 -0500

    adapt to new rest api change

--- packages/composables/src/helpers/populateFeaturedProducts.ts
+++ packages/composables/src/helpers/populateFeaturedProducts.ts
@@ -1,14 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const populateFeaturedProducts = (psProducts: Array<any>) => {
   const populateFeaturedProducts = psProducts.map((product) => ({
-    id: product.id,
+    id: product.id_product,
     name: product.name,
     slug: product.link_rewrite,
     regularPrice: product.regular_price_amount,
     discountPrice: product.price_amount,
-    coverImageSmall: product.cover.small.url,
-    coverImageMedium: product.cover.medium.url,
-    coverImageLarge: product.cover.large.url,
+    coverImageSmall: product.cover.url,
+    coverImageMedium: product.cover.url,
+    coverImageLarge: product.cover.url,
     images: [],
     description: null
   }));
