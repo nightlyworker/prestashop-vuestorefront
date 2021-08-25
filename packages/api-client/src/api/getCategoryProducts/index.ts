16d5b46d40df20c3bf38dec2217c46379f48e842
Sam <sam@binshops.com>
2021-08-25 15:32:19 -0500

    updates getCategoryProducts method

--- packages/api-client/src/api/getCategoryProducts/index.ts
+++ packages/api-client/src/api/getCategoryProducts/index.ts
@@ -1,15 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function getCategoryProducts(context, params) {
-  console.log('inside: getCategoryProducts *****');
-  console.log(params);
-  // const url = new URL('/rest/productdetail', context.config.api.url);
+  const url = new URL('/rest/categoryProducts', context.config.api.url);
 
-  // params.id && url.searchParams.set('product_id', params.id);
-  // params.catId && url.searchParams.set('catId', params.catId);
-  // params.limit && url.searchParams.set('limit', params.limit);
+  url.searchParams.set('slug', params.input.categorySlug);
+  url.searchParams.set('with_all_images', '0');
 
-  // const { data } = await context.client.get(url.href);
+  const { data } = await context.client.get(url.href);
 
-  // return data;
-  return [];
+  return data;
 }
